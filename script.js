6c6d2426b807e38081ffb82fae974c87d6d65508
Arul Pratosh <100361432+Vineesh-Arul-Pratosh@users.noreply.github.com>
2022-04-21 13:47:05 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// .from, .to
+gsap.from('.content',{
+  delay: 1,
+  duration: 2,
+  ease: 'expo.inOut',
+  scale: 2.5,
+});
+
+gsap.from('.nav-bar img', {
+  delay: 2,
+  x : -100,
+  duration: 1,
+  ease: 'expo.inOut',
+  opacity: 0,
+  
+})
+
+gsap.from('#MAIN', {
+  delay: 4,
+  y : -50,
+  duration: 1,
+  ease: 'expo.inOut',
+  opacity: 0,
+  
+})
+
+gsap.from('.DIV1 p', {
+  delay: 5,
+  y : -50,
+  duration: 1,
+  ease: 'expo.inOut',
+  opacity: 0,
+  
+})
+
+gsap.from('.B2', {
+  delay: 6,
+  y : -50,
+  duration: 1,
+  ease: 'expo.inOut',
+  opacity: 0,
+  
+})
+
+gsap.from('.B1', {
+  delay: 3,
+  x : -100,
+  duration: 1,
+  ease: 'expo.inOut',
+  opacity: 0,
+  
+})
